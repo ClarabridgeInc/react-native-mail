5d3337ffec0408093ad4f959f1d6cf3201b1b640
Mikita Siadykh <mikita_siadykh@epam.com>
2020-04-13 09:08:47 +0300

    android - multiple attachements

--- android/src/main/java/com/chirag/RNMail/RNMailModule.java
+++ android/src/main/java/com/chirag/RNMail/RNMailModule.java
@@ -1,31 +1,28 @@
 package com.chirag.RNMail;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.text.Html;
-import android.util.Log;
+import androidx.core.content.FileProvider;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.Callback;
 
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.io.File;
 
 /**
-* NativeModule that allows JS to open emails sending apps chooser.
-*/
-
+ * NativeModule that allows JS to open emails sending apps chooser.
+ */
 public class RNMailModule extends ReactContextBaseJavaModule {
+
   ReactApplicationContext reactContext;
 
   public RNMailModule(ReactApplicationContext reactContext) {
@@ -58,68 +55,76 @@ public class RNMailModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void mail(ReadableMap options, Callback callback) {
-    try {
-      String intentAction = Intent.ACTION_SENDTO;
-      ArrayList<Uri> fileAttachmentUriList = getFileAttachmentUriList(options);
+    String intentAction = Intent.ACTION_SENDTO;
+    ArrayList<Uri> fileAttachmentUriList = getFileAttachmentUriList(options);
 
-      if (1 <= fileAttachmentUriList.size()) {
-        intentAction = Intent.ACTION_SEND_MULTIPLE;
+    if (1 <= fileAttachmentUriList.size()) {
+      intentAction = Intent.ACTION_SEND_MULTIPLE;
+    }
+
+    Intent intent = new Intent(intentAction);
+    intent.setData(Uri.parse("mailto:"));
+
+    if (hasProperty(options, "subject")) {
+      intent.putExtra(Intent.EXTRA_SUBJECT, options.getString("subject"));
+    }
+
+    if (hasProperty(options, "body")) {
+      String body = options.getString("body");
+      if (options.hasKey("isHTML") && options.getBoolean("isHTML")) {
+        intent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(body));
+      } else {
+        intent.putExtra(Intent.EXTRA_TEXT, body);
       }
+    }
 
-      Intent intent = new Intent(intentAction);
-      intent.setData(Uri.parse("mailto:"));
+    if (hasProperty(options, "recipients")) {
+      ReadableArray recipients = options.getArray("recipients");
+      intent.putExtra(Intent.EXTRA_EMAIL, readableArrayToStringArray(recipients));
+    }
 
-      if (hasKeyAndDefined(options, "subject")) {
-        intent.putExtra(Intent.EXTRA_SUBJECT, options.getString("subject"));
+    if (hasProperty(options, "ccRecipients")) {
+      ReadableArray ccRecipients = options.getArray("ccRecipients");
+      intent.putExtra(Intent.EXTRA_CC, readableArrayToStringArray(ccRecipients));
+    }
+
+    if (hasProperty(options, "bccRecipients")) {
+      ReadableArray bccRecipients = options.getArray("bccRecipients");
+      intent.putExtra(Intent.EXTRA_BCC, readableArrayToStringArray(bccRecipients));
+    }
+
+    if (1 <= fileAttachmentUriList.size()) {
+      intent.setType("text/plain");
+      intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, fileAttachmentUriList);
+      intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+    }
+
+    PackageManager manager = reactContext.getPackageManager();
+    List<ResolveInfo> list = manager.queryIntentActivities(intent, 0);
+
+    if (list == null || list.size() == 0) {
+      callback.invoke("not_available");
+      return;
+    }
+
+    if (list.size() == 1) {
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      try {
+        reactContext.startActivity(intent);
+        callback.invoke(fileAttachmentUriList.toString());
+      } catch (Exception ex) {
+        callback.invoke("error activity", ex.toString(), ex.getMessage());
       }
-
-      // On Android HTML email body results are awful, no <table>,<ol>,etc.
-      // Is there really no way to make it work like iOS?
-      // http://blog.iangclifton.com/2010/05/17/sending-html-email-with-android-intent/
-      // http://www.nowherenearithaca.com/2011/10/some-notes-for-sending-html-email-in.html
-      if (hasKeyAndDefined(options, "body")) {
-        String body = options.getString("body");
-        if (options.hasKey("isHTML") && options.getBoolean("isHTML")) {
-          intent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml(body));
-        } else {
-          intent.putExtra(Intent.EXTRA_TEXT, body);
-        }
-      }
-
-      if (hasKeyAndDefined(options, "recipients")) {
-        ReadableArray recipients = options.getArray("recipients");
-        intent.putExtra(Intent.EXTRA_EMAIL, readableArrayToStringArray(recipients));
-      }
-
-      if (hasKeyAndDefined(options, "ccRecipients")) {
-        ReadableArray ccRecipients = options.getArray("ccRecipients");
-        intent.putExtra(Intent.EXTRA_CC, readableArrayToStringArray(ccRecipients));
-      }
-
-      if (hasKeyAndDefined(options, "bccRecipients")) {
-        ReadableArray bccRecipients = options.getArray("bccRecipients");
-        intent.putExtra(Intent.EXTRA_BCC, readableArrayToStringArray(bccRecipients));
-      }
-
-      if (1 <= fileAttachmentUriList.size()) {
-        // If multiple attachments setType("plain/text"), else queryIntentActivities fails
-        intent.setType("plain/text");
-        intent.putExtra(Intent.EXTRA_STREAM, fileAttachmentUriList);
-      }
-
-      PackageManager manager = reactContext.getPackageManager();
-      List<ResolveInfo> list = manager.queryIntentActivities(intent, 0);
-      if (list == null || list.size() == 0) {
-        callback.invoke("not_available");
-        return;
-      }
-
+    } else {
       Intent chooser = Intent.createChooser(intent, "Send Mail");
       chooser.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      reactContext.startActivity(chooser);
 
-    } catch (Exception ex) {
-      callback.invoke("error RNMail:mail");
+      try {
+        reactContext.startActivity(chooser);
+        callback.invoke(fileAttachmentUriList.toString() + options.getArray("attachments").size());
+      } catch (Exception ex) {
+        callback.invoke("error chooser" + ex.toString() + ex.getMessage());
+      }
     }
   }
 
@@ -129,7 +134,7 @@ public class RNMailModule extends ReactContextBaseJavaModule {
   * @param @{link String} key
   * @return boolean representing whether the key exists and has a value
   */
-  private Boolean hasKeyAndDefined(ReadableMap readableMap, String key) {
+  private Boolean hasProperty(ReadableMap readableMap, String key) {
     return readableMap.hasKey(key) && !readableMap.isNull(key);
   }
 
@@ -141,35 +146,24 @@ public class RNMailModule extends ReactContextBaseJavaModule {
   private ArrayList<Uri> getFileAttachmentUriList(ReadableMap options) {
     ArrayList<Uri> fileAttachmentUriList = new ArrayList<Uri>();
 
-    if (hasKeyAndDefined(options, "attachmentList")) {
-      ReadableArray attachmentList = options.getArray("attachmentList");
+    if (hasProperty(options, "attachments")) {
+      ReadableArray attachmentList = options.getArray("attachments");
       int length = attachmentList.size();
 
-      for(int i = 0; i < length; ++i) {
+      for(int i = 0; i < length; i++) {
         ReadableMap attachmentItem = attachmentList.getMap(i);
-        String fileName = "";
-        String path = "";
-        String mimeType = "";
+        String path = attachmentItem.getString("path");
 
-        if (hasKeyAndDefined(attachmentItem, "name")) {
-          fileName = attachmentItem.getString("name");
-        }
+        File file = new File(path);
+        Uri apkURI = FileProvider.getUriForFile(
+                                 reactContext,
+                                 reactContext.getApplicationContext()
+                                 .getPackageName() + ".provider", file);
 
-        if (hasKeyAndDefined(attachmentItem, "path")) {
-          path = attachmentItem.getString("path");
-        }
-
-        if (hasKeyAndDefined(attachmentItem, "mimeType")) {
-          mimeType = attachmentItem.getString("mimeType");
-        }
-
-        boolean fileExists = new File(path).exists();
-
-        if (fileExists) {
-          fileAttachmentUriList.add(Uri.parse("file://" + path));
-        }
+        fileAttachmentUriList.add(apkURI);
       }
     }
+
     return fileAttachmentUriList;
   }
 }
